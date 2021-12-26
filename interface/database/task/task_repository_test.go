6fe830e3d1c2ba766a0f6ac94314daed34ffeab8
hajime.takeuchi <shadowthe8156@gmail.com>
2021-12-26 12:18:19 +0900

    task_repositoryテスト作成

--- interface/database/task/task_repository_test.go
+++ interface/database/task/task_repository_test.go
@@ -2,6 +2,7 @@ package task_test
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"regexp"
 	"testing"
@@ -26,29 +27,25 @@ func TestFindByUserID(t *testing.T) {
 
 	repo := taskRepository.NewTaskRepository(sqlDriver)
 
+	query := `
+		SELECT
+			*
+		FROM
+			tasks
+		WHERE
+			user_id = ?
+		ORDER BY
+			due_date
+		LIMIT ? OFFSET ?
+	`
+
 	t.Run("正常系 指定したユーザーIDのみすべて取得されていること", func(t *testing.T) {
 		userID := int64(1)
-
 		rows := sqlmock.NewRows([]string{"id", "user_id", "title", "content", "due_date", "updated_at", "created_at"})
-
-		// ユーザーID=1でデータを5件作成
 		mockTasks := createMockTasks(5, userID)
 		for _, mockTask := range mockTasks {
 			rows.AddRow(mockTask.ID, mockTask.UserID, mockTask.Title, mockTask.Content, mockTask.DueDate, mockTask.UpdatedAt, mockTask.CreatedAt)
 		}
-		//rows.AddRow(int64(6), int64(2), "dummy title", "dummy content", time.Now(), time.Now(), time.Now())
-
-		query := `
-			SELECT
-				*
-			FROM
-				tasks
-			WHERE
-				user_id = ?
-			ORDER BY
-				due_date
-			LIMIT ? OFFSET ?
-		`
 		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(userID, 3, 1).WillReturnRows(rows)
 
 		got, err := repo.FindByUserID(context.TODO(), userID, 3, 1)
@@ -56,29 +53,46 @@ func TestFindByUserID(t *testing.T) {
 		assert.Equal(t, mockTasks, got)
 	})
 
-	t.Run("正常系 指定した範囲のデータが取得できていること", func(t *testing.T) {
-	})
-
 	t.Run("準正常系 データが存在しない場合、エラーとならないこと", func(t *testing.T) {
 		userID := int64(1)
-
 		rows := sqlmock.NewRows([]string{"id", "user_id", "title", "content", "due_date", "updated_at", "created_at"})
-		query := `
-			SELECT
-				*
-			FROM
-				tasks
-			WHERE
-				user_id = ?
-			ORDER BY
-				due_date
-			LIMIT ? OFFSET ?
-		`
 		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(userID, 5, 0).WillReturnRows(rows)
 
 		got, err := repo.FindByUserID(context.TODO(), userID, 5, 0)
 		assert.NoError(t, err)
 		assert.Equal(t, []domain.Task{}, got)
+	})
+
+	t.Run("異常系 クエリ実行で失敗した場合エラーが返却されること", func(t *testing.T) {
+		userID := int64(1)
+		mockErr := errors.New("query failed error")
+		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(userID, 5, 0).
+			WillReturnError(mockErr)
+
+		got, err := repo.FindByUserID(context.TODO(), userID, 5, 0)
+		assert.Equal(t, mockErr, err)
+		assert.Nil(t, got)
+	})
+
+	t.Run("異常系 Scan実行で失敗した場合エラーが返却されること", func(t *testing.T) {
+		userID := int64(1)
+		rows := sqlmock.NewRows([]string{"foo"}).AddRow("bar")
+		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(userID, 5, 0).WillReturnRows(rows)
+
+		got, err := repo.FindByUserID(context.TODO(), userID, 5, 0)
+		assert.NotNil(t, err)
+		assert.Nil(t, got)
+	})
+
+	t.Run("異常系 Rows.Close実行で失敗した場合にログが出力されること", func(t *testing.T) {
+		userID := int64(1)
+		mockErr := errors.New("rows close error")
+		rows := sqlmock.NewRows([]string{"foo"}).AddRow("bar").CloseError(mockErr)
+		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(userID, 5, 0).WillReturnRows(rows)
+
+		got, err := repo.FindByUserID(context.TODO(), userID, 5, 0)
+		assert.NotNil(t, err)
+		assert.Nil(t, got)
 	})
 }
 
@@ -102,12 +116,11 @@ func TestGetByID(t *testing.T) {
 	}
 
 	repo := taskRepository.NewTaskRepository(sqlDriver)
+	query := "SELECT * FROM tasks WHERE id = ?"
 
 	t.Run("正常系 存在するIDで1件取得", func(t *testing.T) {
 		rows := sqlmock.NewRows([]string{"id", "user_id", "title", "content", "due_date", "updated_at", "created_at"}).
 			AddRow(mockTask.ID, mockTask.UserID, mockTask.Title, mockTask.Content, mockTask.DueDate, mockTask.UpdatedAt, mockTask.CreatedAt)
-
-		query := "SELECT * FROM tasks WHERE id = ?"
 		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(mockTask.ID).WillReturnRows(rows)
 
 		got, err := repo.GetByID(context.TODO(), mockTask.ID)
@@ -117,67 +130,167 @@ func TestGetByID(t *testing.T) {
 
 	t.Run("準正常系 存在しないIDで検索してエラーとなること", func(t *testing.T) {
 		rows := sqlmock.NewRows([]string{"id", "user_id", "title", "content", "due_date", "updated_at", "created_at"})
-
-		query := "SELECT * FROM tasks WHERE id = ?"
 		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(int64(2)).WillReturnRows(rows)
 
 		got, err := repo.GetByID(context.TODO(), int64(2))
 		assert.Equal(t, domain.ErrRecordNotFound, err)
 		assert.Equal(t, domain.Task{}, got)
 	})
+
+	t.Run("異常系 クエリ実行で失敗した場合エラーが返却されること", func(t *testing.T) {
+		mockErr := errors.New("query failed error")
+		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(int64(2)).WillReturnError(mockErr)
+
+		got, err := repo.GetByID(context.TODO(), int64(2))
+		assert.Equal(t, mockErr, err)
+		assert.Equal(t, domain.Task{}, got)
+	})
+
+	t.Run("異常系 Scan実行で失敗した場合エラーが返却されること", func(t *testing.T) {
+		rows := sqlmock.NewRows([]string{"foo"}).AddRow("bar")
+		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(int64(2)).WillReturnRows(rows)
+
+		got, err := repo.GetByID(context.TODO(), int64(2))
+		assert.NotNil(t, err)
+		assert.Equal(t, domain.Task{}, got)
+	})
+
+	t.Run("異常系 Rows.Close実行で失敗した場合にログが出力されること", func(t *testing.T) {
+		mockErr := errors.New("rows close error")
+		rows := sqlmock.NewRows([]string{"foo"}).AddRow("bar").CloseError(mockErr)
+		mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(int64(2)).WillReturnRows(rows)
+
+		got, err := repo.GetByID(context.TODO(), int64(2))
+		assert.NotNil(t, err)
+		assert.Equal(t, domain.Task{}, got)
+	})
 }
 
 func TestCreate(t *testing.T) {
-	// db, mock, err := sqlmock.New()
-	// if err != nil {
-	// 	t.Fatalf("sqlmock error: '%s'", err)
-	// }
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("sqlmock error: '%s'", err)
+	}
 
-	// sqlDriver := new(infrastructure.SqlDriver)
-	// sqlDriver.Conn = db
+	sqlDriver := new(infrastructure.SqlDriver)
+	sqlDriver.Conn = db
 
-	// repo := taskRepository.NewTaskRepository(sqlDriver)
+	repo := taskRepository.NewTaskRepository(sqlDriver)
+	query := "INSERT INTO tasks(user_id,title,content,due_date) VALUES(?,?,?,?)"
 
-	t.Run("正常系 存在するIDで1件取得", func(t *testing.T) {
+	t.Run("正常系 1件追加", func(t *testing.T) {
+		mockTask := domain.Task{
+			UserID:  1,
+			Title:   "test title",
+			Content: "test content",
+			DueDate: time.Now(),
+		}
+		prep := mock.ExpectPrepare(regexp.QuoteMeta(query))
+		prep.ExpectExec().
+			WithArgs(mockTask.UserID, mockTask.Title, mockTask.Content, mockTask.DueDate).
+			WillReturnResult(sqlmock.NewResult(12, 1))
+
+		err = repo.Create(context.TODO(), mockTask)
+		assert.NoError(t, err)
 	})
 
-	t.Run("準正常系 存在しないIDで検索してエラーとなること", func(t *testing.T) {
+	t.Run("異常系 クエリ実行で失敗した場合エラーが返却されること", func(t *testing.T) {
+		mockTask := domain.Task{
+			UserID:    1,
+			Title:     "test title",
+			Content:   "test content",
+			DueDate:   time.Now(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+		mockErr := errors.New("query failed error")
+		prep := mock.ExpectPrepare(regexp.QuoteMeta(query))
+		prep.ExpectExec().
+			WithArgs(mockTask.UserID, mockTask.Title, mockTask.Content, mockTask.DueDate).
+			WillReturnError(mockErr)
+
+		err = repo.Create(context.TODO(), mockTask)
+		assert.Equal(t, mockErr, err)
 	})
 }
 
 func TestUpdate(t *testing.T) {
-	// db, mock, err := sqlmock.New()
-	// if err != nil {
-	// 	t.Fatalf("sqlmock error: '%s'", err)
-	// }
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("sqlmock error: '%s'", err)
+	}
 
-	// sqlDriver := new(infrastructure.SqlDriver)
-	// sqlDriver.Conn = db
+	sqlDriver := new(infrastructure.SqlDriver)
+	sqlDriver.Conn = db
 
-	// repo := taskRepository.NewTaskRepository(sqlDriver)
+	repo := taskRepository.NewTaskRepository(sqlDriver)
+	query := "UPDATE tasks SET title = ?, content = ?, due_date = ? where id = ?"
 
-	t.Run("正常系 存在するIDで1件取得", func(t *testing.T) {
+	t.Run("正常系 1件更新", func(t *testing.T) {
+		mockTask := domain.Task{
+			ID:      1,
+			Title:   "test title",
+			Content: "test content",
+			DueDate: time.Now(),
+		}
+		prep := mock.ExpectPrepare(regexp.QuoteMeta(query))
+		prep.ExpectExec().
+			WithArgs(mockTask.Title, mockTask.Content, mockTask.DueDate, mockTask.ID).
+			WillReturnResult(sqlmock.NewResult(12, 1))
+
+		err = repo.Update(context.TODO(), mockTask)
+		assert.NoError(t, err)
 	})
 
-	t.Run("準正常系 存在しないIDで検索してエラーとなること", func(t *testing.T) {
+	t.Run("異常系 クエリ実行で失敗した場合エラーが返却されること", func(t *testing.T) {
+		mockTask := domain.Task{
+			ID:      1,
+			Title:   "test title",
+			Content: "test content",
+			DueDate: time.Now(),
+		}
+		mockErr := errors.New("query failed error")
+		prep := mock.ExpectPrepare(regexp.QuoteMeta(query))
+		prep.ExpectExec().
+			WithArgs(mockTask.Title, mockTask.Content, mockTask.DueDate, mockTask.ID).
+			WillReturnError(mockErr)
+
+		err = repo.Update(context.TODO(), mockTask)
+		assert.Equal(t, mockErr, err)
 	})
 }
 
 func TestDelete(t *testing.T) {
-	// db, mock, err := sqlmock.New()
-	// if err != nil {
-	// 	t.Fatalf("sqlmock error: '%s'", err)
-	// }
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("sqlmock error: '%s'", err)
+	}
 
-	// sqlDriver := new(infrastructure.SqlDriver)
-	// sqlDriver.Conn = db
+	sqlDriver := new(infrastructure.SqlDriver)
+	sqlDriver.Conn = db
 
-	// repo := taskRepository.NewTaskRepository(sqlDriver)
+	repo := taskRepository.NewTaskRepository(sqlDriver)
+	query := "DELETE FROM tasks where id = ? "
 
-	t.Run("正常系 存在するIDで1件取得", func(t *testing.T) {
+	t.Run("正常系 1件削除", func(t *testing.T) {
+		prep := mock.ExpectPrepare(regexp.QuoteMeta(query))
+		prep.ExpectExec().
+			WithArgs(int64(1)).
+			WillReturnResult(sqlmock.NewResult(12, 1))
+
+		err = repo.Delete(context.TODO(), int64(1))
+		assert.NoError(t, err)
 	})
 
-	t.Run("準正常系 存在しないIDで検索してエラーとなること", func(t *testing.T) {
+	t.Run("異常系 クエリ実行で失敗した場合エラーが返却されること", func(t *testing.T) {
+		mockErr := errors.New("query failed error")
+		prep := mock.ExpectPrepare(regexp.QuoteMeta(query))
+		prep.ExpectExec().
+			WithArgs(int64(1)).
+			WillReturnError(mockErr)
+
+		err = repo.Delete(context.TODO(), int64(1))
+		assert.Equal(t, mockErr, err)
 	})
 }
 
