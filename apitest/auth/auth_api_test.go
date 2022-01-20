9010548fc2f7fc2466d531874e92c538dbe08bd3
hajime.takeuchi <shadowthe8156@gmail.com>
2022-01-21 01:43:47 +0900

    signInAPIテスト作成

--- apitest/auth/auth_api_test.go
+++ apitest/auth/auth_api_test.go
@@ -3,6 +3,7 @@ package auth_test
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -23,7 +24,7 @@ import (
 
 const signUpURL = "http://localhost:8080/auth/sign_up"
 
-// const signInURL = "http://localhost:8080/auth/sign_in"
+const signInURL = "http://localhost:8080/auth/sign_in"
 
 var sqlDriver interfaceDB.SqlDriver
 
@@ -162,11 +163,142 @@ func TestSignUp(t *testing.T) {
 }
 
 func TestSignIn(t *testing.T) {
-	t.Run("正常系 ユーザーの新規登録後、サインインできること", func(t *testing.T) {})
-	t.Run("準正常系 存在しないEmailの場合、401エラーとなること", func(t *testing.T) {})
-	t.Run("準正常系 パスワードが間違っている場合、401エラーとなること", func(t *testing.T) {})
-	t.Run("準正常系 リクエストパラメータが足りていない場合、400エラーとなること", func(t *testing.T) {})
-	t.Run("準正常系 リクエスト形式が間違っている場合、400エラーとなること", func(t *testing.T) {})
+	t.Run("正常系 ユーザーの新規登録後、サインインできること", func(t *testing.T) {
+		ctx := context.TODO()
+		password := "password"
+		createUser := createTestUser(password)
+		repo := userRepository.NewUserRepository(sqlDriver)
+		createdID, _ := repo.Create(ctx, createUser)
+
+		request := authHandler.SignInRequest{
+			Email:    createUser.Email,
+			Password: password,
+		}
+		byteRequest, _ := json.Marshal(request)
+		req, _ := http.NewRequest("POST", signInURL, bytes.NewBuffer(byteRequest))
+		req.Header.Set("Content-Type", "application/json")
+		client := new(http.Client)
+		res, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		var response authHandler.SignInResponse
+		decoder := json.NewDecoder(res.Body)
+		err = decoder.Decode(&response)
+		if err != nil {
+			t.Fatal(err)
+		}
+		userID := getUserIDFromToken(response.Token)
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+		assert.Equal(t, createdID, userID)
+	})
+	t.Run("準正常系 存在しないEmailの場合、401エラーとなること", func(t *testing.T) {
+		request := authHandler.SignInRequest{
+			Email:    generateRandomEmail(),
+			Password: "password",
+		}
+		byteRequest, _ := json.Marshal(request)
+		req, _ := http.NewRequest("POST", signInURL, bytes.NewBuffer(byteRequest))
+		req.Header.Set("Content-Type", "application/json")
+		client := new(http.Client)
+		res, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		var response domain.ErrorResponse
+		decoder := json.NewDecoder(res.Body)
+		err = decoder.Decode(&response)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
+		assert.Equal(t, domain.ErrorResponse{Message: domain.ErrFailedSignIn.Error()}, response)
+	})
+	t.Run("準正常系 パスワードが間違っている場合、401エラーとなること", func(t *testing.T) {
+		ctx := context.TODO()
+		password := "password"
+		createUser := createTestUser(password)
+		repo := userRepository.NewUserRepository(sqlDriver)
+		repo.Create(ctx, createUser)
+
+		request := authHandler.SignInRequest{
+			Email:    generateRandomEmail(),
+			Password: "mismatch password",
+		}
+		byteRequest, _ := json.Marshal(request)
+		req, _ := http.NewRequest("POST", signInURL, bytes.NewBuffer(byteRequest))
+		req.Header.Set("Content-Type", "application/json")
+		client := new(http.Client)
+		res, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		var response domain.ErrorResponse
+		decoder := json.NewDecoder(res.Body)
+		err = decoder.Decode(&response)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
+		assert.Equal(t, domain.ErrorResponse{Message: domain.ErrFailedSignIn.Error()}, response)
+	})
+	t.Run("準正常系 リクエストパラメータが足りていない場合、400エラーとなること", func(t *testing.T) {
+		request := authHandler.SignInRequest{
+			Email: generateRandomEmail(),
+		}
+		byteRequest, _ := json.Marshal(request)
+		req, _ := http.NewRequest("POST", signInURL, bytes.NewBuffer(byteRequest))
+		req.Header.Set("Content-Type", "application/json")
+		client := new(http.Client)
+		res, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		var response domain.ErrorResponse
+		decoder := json.NewDecoder(res.Body)
+		err = decoder.Decode(&response)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+		assert.NotEmpty(t, response.Message)
+	})
+	t.Run("準正常系 リクエスト形式が間違っている場合、400エラーとなること", func(t *testing.T) {
+		request := domain.ErrorResponse{
+			Message: "foo",
+		}
+		byteRequest, _ := json.Marshal(request)
+		req, _ := http.NewRequest("POST", signInURL, bytes.NewBuffer(byteRequest))
+		req.Header.Set("Content-Type", "application/json")
+		client := new(http.Client)
+		res, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		var response domain.ErrorResponse
+		decoder := json.NewDecoder(res.Body)
+		err = decoder.Decode(&response)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+		assert.NotEmpty(t, response.Message)
+	})
 }
 
 func getUserIDFromToken(strToken string) int64 {
@@ -177,6 +309,35 @@ func getUserIDFromToken(strToken string) int64 {
 	return claims.UserID
 }
 
+// createTestUser 指定したパスワードでテスト用のユーザーオブジェクトを作成します
+func createTestUser(password string) domain.User {
+	email := generateRandomEmail()
+	salt := generateSalt()
+	pass := []byte(password + salt)
+	hashed, _ := bcrypt.GenerateFromPassword(pass, bcrypt.DefaultCost)
+	return domain.User{
+		Name:     "test name",
+		Email:    email,
+		Password: string(hashed),
+		Salt:     salt,
+	}
+}
+
+// generateRandomEmail テスト用のランダムなメールアドレスを作成します
 func generateRandomEmail() string {
 	return fmt.Sprintf("%d@example.com", time.Now().UnixNano())
+}
+
+// generateSalt salt(10桁のランダム文字列)を作成します
+func generateSalt() string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	b := make([]byte, 10)
+	rand.Read(b)
+
+	var result string
+	for _, v := range b {
+		result += string(letters[int(v)%len(letters)])
+	}
+	return result
 }
